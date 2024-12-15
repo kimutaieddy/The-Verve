6871f4f2f005d7b27abd0b172a1c77cbbc95a2e6
Kimutaieddy <eddykimutai@outlook.com>
2024-12-15 11:25:44 +0300

    Add PDF fetching functionality to Resources component

--- client-end/src/Resources.jsx
+++ client-end/src/Resources.jsx
@@ -1,7 +1,26 @@
-import  React, {useState, } from "react";
+import  React, {useState,useEffect } from "react";
 import  './Resources.css';
 
 function Resources() {
-    const [pdf,setPdfs] =useState
+    const [pdf,setPdfs] =useState ([]);
+    const [selectedpdf,setSelectedFile] =useState(null);
+
+    useEffect ( () => {
+        fetchPdfs();
+    } ,[]
+
+    );
+
+    const fetchPdfs =async () => {
+        try {
+            const response =await fetch ('http://your-drf-api-endpoint/pdfs/');
+            const data =awit response.json();
+            setPdfs(data);
+        }
+        catch (error) {
+            console.log('Error fetching PDFs:',error)
+        }
+    } ;
+    
     
 }
