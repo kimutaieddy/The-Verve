b91aa8bb89f03c32499751f6e263929b80ab89b7
Kimutaieddy <eddykimutai@outlook.com>
2024-12-22 22:14:22 +0300

    Add PDF serializer for API data handling

---
+++ backend/api/serializers.py
@@ -0,0 +1,9 @@
+# api/serializers.py
+
+from rest_framework import serializers
+from .models import PDF
+
+class PDFSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PDF
+        fields = ('id', 'name', 'file', 'url')
