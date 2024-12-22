6a4174c38bced65c30607579d2a1d9bdaae0b6f1
Kimutaieddy <eddykimutai@outlook.com>
2024-12-22 22:13:28 +0300

    Add PDF model with name and file fields

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class PDF(models.Model):
+    name = models.CharField(max_length=255)
+    file = models.FileField(upload_to='pdfs/')
+
+    @property
+    def url(self):
+        return self.file.url
