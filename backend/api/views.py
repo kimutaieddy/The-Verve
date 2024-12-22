935e6e2fb79015a974c8335f870573c7e18c274d
Kimutaieddy <eddykimutai@outlook.com>
2024-12-22 20:46:12 +0300

    Add PdfViewSet with MultiPartParser for handling PDF uploads

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,2 +1,9 @@
 from rest_framework import viewsets
 from rest_framework.parsers import MultiPartParser
+from .models import pdfs
+from .serializers import PdfSerializer
+
+class PdfViewSet(viewsets.ModelViewSet):
+    queryset = pdfs.objects.all()
+    serializer_class = PdfSerializer
+    parser_classes = (MultiPartParser,formprarsers)
