76782a22c07e724e4eb686f34da2a2ae484b2a05
Kimutaieddy <eddykimutai@outlook.com>
2024-12-22 21:43:22 +0300

    Fix routing configuration for PdfViewSet in API URL patterns

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -15,12 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from api.views import PdfViewSet
 
+router = DefaultRouter()
+router.register(r'pdfs', PdfViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/' include(router.urls),)
+    path('api/', include(router.urls)),
 ]
