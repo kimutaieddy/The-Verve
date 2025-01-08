8b861aee79ba7f5a4f1cf24702bdbba19b460b78
Kimutaieddy <eddykimutai@outlook.com>
2025-01-08 11:50:35 +0300

    Change database engine to SQLite and update database settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,12 +37,8 @@ from decouple import config
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': config('DATABASE_NAME'),
-        'USER': config('DATABASE_USER'),
-        'PASSWORD': config('DATABASE_PASSWORD'),
-        'HOST': config('DATABASE_HOST'),
-        'PORT': config('DATABASE_PORT'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
